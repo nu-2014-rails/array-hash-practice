d0cb64fa80fc6d866bdcf6f8e90f69ba14e69a58
Arjun Venkataswamy <you@example.com>
2014-10-21 20:45:07 +0000

    first commit

---
+++ practice.rb
@@ -0,0 +1,17 @@
+# remember, the command "puts" will display stuff on the screen
+
+# pull out the word "apple"" from the following array
+fruits = ["apple", "orange", "banana"]
+
+# pull out the word "Yay!" from the following hash
+sports = {"Cubs" => "yay!", "Sox" => "booo!"}
+
+# pull out Obama's street address from the personal_info variable
+personal_info = [{
+    "name" => {"first" = "Barack", "last" => "Obama"},
+    "address" => {
+      "street" => "1600 Pennsylvania Ave NW",
+      "city" => "Washington",
+      "state" => "D.C."
+    }
+}]
